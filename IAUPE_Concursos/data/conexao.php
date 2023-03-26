c59d27e2ca1b1e8ee9b7dc9c76237972f0ae46b9
Helifaz Rocha <76110177+helifazdev@users.noreply.github.com>
2023-03-26 18:51:16 -0300

    ERRO de Pasta Resolvido

--- IAUPE_Concursos/data/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('USUARIO', 'HELIFAZ');
-define('SENHA', 'PROJETOIC');
-define('DB', 'LOGIN_CAD_IC');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('não foi possível conectar');
--- IAUPE_Concursos/js/main.js
+++
@@ -1,7 +0,0 @@
-//var versao = ("Concurso25032023")
-//alert("Olá obrigado por nos visitar!\nO site ainda está em desenvolvimento \nVersão Atual: " + versao + "\nDev:helifaz Rocha");
-
-//setInterval(function(){
-//    location.reload();
-//  }, 30000); //atualiza a página a cada 5 segundos
-
